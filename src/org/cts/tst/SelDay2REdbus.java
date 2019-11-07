2a1cf9d8d9aed8a18733b46b3dae7801d24f4750
karthikeyankandhan <karthikeyankandhan@gmail.com>
2019-11-07 11:37:13 +0530

    done smithin

--- src/org/cts/tst/SelDay2REdbus.java
+++ src/org/cts/tst/SelDay2REdbus.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SelDay2REdbus {
 	
@@ -26,5 +27,22 @@ public static void main(String[] args) {
 	pass.sendKeys("pondicvherry");
 	
 	
+	
+	System.setProperty("webdriver.gecko.driver",
+			"C:\\Users\\pr soft\\eclipse-workspace\\karthikeyankandhan\\Sample\\driver\\geckodriver23.exe");
+
+	WebDriver a = new FirefoxDriver();
+
+	a.get("https://www.facebook.com/");
+	String title = a.getTitle();
+	System.out.println(title);
+	String currentUrl = a.getCurrentUrl();
+	System.out.println(currentUrl);
+
+	WebElement findElement = a.findElement(By.id("email"));
+	findElement.sendKeys("abc@gmailcom");
+	WebElement findEle = a.findElement(By.id("pass"));
+
+	findEle.sendKeys("12345");
 }
 }
