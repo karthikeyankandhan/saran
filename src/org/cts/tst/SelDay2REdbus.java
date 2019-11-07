1e28dc17ce64f59588f19ddc2c27f1a2da3e6e28
karthikeyankandhan <karthikeyankandhan@gmail.com>
2019-11-07 11:46:54 +0530

    w

--- src/org/cts/tst/SelDay2REdbus.java
+++ src/org/cts/tst/SelDay2REdbus.java
@@ -11,6 +11,9 @@ public class SelDay2REdbus {
 
 public static void main(String[] args) {
 	
+	
+	int id = 10;
+	
 
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\pr soft\\eclipse-workspace\\karthikeyankandhan\\SelDay2Testq2\\selDriver\\chromedriver76Version.exe");
 	
@@ -31,7 +34,7 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.gecko.driver",
 			"C:\\Users\\pr soft\\eclipse-workspace\\karthikeyankandhan\\Sample\\driver\\geckodriver23.exe");
 
-	WebDriver a = new FirefoxDriver();
+	WebDriver a = new FilrefoxDriver();
 
 	a.get("https://www.facebook.com/");
 	String title = a.getTitle();
